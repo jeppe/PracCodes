c59e0b947b4693cbfd2b2d8542bb976d8ee9a0a3
yu lu <coolluyu@gmail.com>
2014-05-08 16:53:28 +0800

    basic language elements

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,11 +1,14 @@
-#include <iostream>
-
-using namespace std;
+#include "lang.cpp"
 
 int main(){
 
 	int vari = 0;
-	cout << "hello world!";
-	cin >> vari ;
+	std::string address = "China";
+	std::string address = address;
+
+	int string = 123;
+
+	cout << "hello world!" << address << string;
+	cin >> vari;
 	return 0;
 }
